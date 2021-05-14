b5b95bbf50722457aca7b48f3e401ab849785ce3
Ballester Valios <ballester@minsait.com>
2021-05-14 16:01:54 +0200

    first commit

---
+++ src/test/java/com/minsait/arq/gallery/GalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minsait.arq.gallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
